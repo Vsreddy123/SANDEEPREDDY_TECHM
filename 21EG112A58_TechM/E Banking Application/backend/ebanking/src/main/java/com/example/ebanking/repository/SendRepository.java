2b5d68bc621d9722429725281cfe72c8a5218dc9
SandeepReddy <113415268+Vsreddy123@users.noreply.github.com>
2025-03-18 14:20:49 +0530

    Delete 21EG112A58_TechM/E Banking Application/backend/ebanking directory

--- 21EG112A58_TechM/E Banking Application/backend/ebanking/src/main/java/com/example/ebanking/repository/SendRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ebanking.repository;
-
-import com.example.ebanking.entity.Send;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SendRepository extends JpaRepository<Send, Long> {
-}
